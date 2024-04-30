88ce8bd1d0c26fa92dd2205b1dc75fc987d2ee6e
alexandrequintela <alexsquin@gmail.com>
2024-04-30 04:35:32 -0300

    Ajuste no nome do atributo Logradouro

--- src/DevIO.Business/Models/Endereco.cs
+++ src/DevIO.Business/Models/Endereco.cs
@@ -2,7 +2,7 @@
 
 public class Endereco : Entity
 {
-    public string? Logradoro { get; set; }
+    public string? Logradouro { get; set; }
     public string? Numero { get; set; }
     public string? Complemento { get; set; }
     public string? Cep { get; set; }
