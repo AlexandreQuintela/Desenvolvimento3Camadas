6bd213a820940debc33d1fd75054618209914af6
alexandrequintela <alexsquin@gmail.com>
2024-05-01 04:17:57 -0300

    Criação do método RemoverEnderecoFornecedor na interface FornecedorRepository

--- src/DevIO.Business/Interfaces/IFornecedorRepository.cs
+++ src/DevIO.Business/Interfaces/IFornecedorRepository.cs
@@ -7,4 +7,5 @@ public interface IFornecedorRepository : IRepository<Fornecedor>
     Task<Fornecedor> ObterFornecedorEndereco(Guid id);
     Task<Fornecedor> ObterFornecedorProdutosEndereco(Guid id);
     Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId);
+    Task RemoverEnderecoFornecedor(Endereco endereco);
 }
