9e358b83846194d580ebc10dfdd6fc8a64d88fc5
alexandrequintela <alexsquin@gmail.com>
2024-05-04 07:10:11 -0300

    Mapemento classe produto

---
+++ src/DevIO.Data/Mappings/ProdutoMapping.cs
@@ -0,0 +1,22 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Data.Mappings;
+
+public class ProdutoMapping : IEntityTypeConfiguration<Produto>
+{
+    public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Produto> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.ToTable("Produtos");
+
+        builder.Property(x => x.Nome)
+            .IsRequired()
+            .HasColumnName("varchar(200)");
+
+        builder.Property(x => x.Descricao)
+            .IsRequired()
+            .HasColumnName("varchar(100)");
+    }
+}
