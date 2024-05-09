ebf995fe1c1ff4beb5ee4316968f0add30d5f269
alexandrequintela <alexsquin@gmail.com>
2024-05-09 04:08:07 -0300

    migração para ajuste dos nomes do campos para a tabela produto

---
+++ src/DevIO.Data/Migrations/20240509070623_AjusteNomeCamposProduto.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevIO.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjusteNomeCamposProduto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "varchar(200)",
+                table: "Produtos",
+                newName: "Nome");
+
+            migrationBuilder.RenameColumn(
+                name: "varchar(1000)",
+                table: "Produtos",
+                newName: "Descricao");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Produtos",
+                type: "varchar(200)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(150)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Produtos",
+                type: "varchar(1000)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(150)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Nome",
+                table: "Produtos",
+                newName: "varchar(200)");
+
+            migrationBuilder.RenameColumn(
+                name: "Descricao",
+                table: "Produtos",
+                newName: "varchar(1000)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "varchar(200)",
+                table: "Produtos",
+                type: "varchar(150)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(200)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "varchar(1000)",
+                table: "Produtos",
+                type: "varchar(150)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(1000)");
+        }
+    }
+}
