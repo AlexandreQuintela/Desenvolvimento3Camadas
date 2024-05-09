da5fa59fd4b41a4a70bdf93b05a44f53f333f722
alexandrequintela <alexsquin@gmail.com>
2024-05-09 02:36:27 -0300

    Configuração do dbcontext da aplicação

--- src/DevIO.Data/Context/MeuDbContext.cs
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -9,6 +9,30 @@ public class MeuDbContext : DbContext
     {
     }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var propriedades = modelBuilder.Model.GetEntityTypes()
+                                                    .SelectMany(e => e.GetProperties()
+                                                        .Where(p => p.ClrType == typeof(string)));
+        foreach (var propiedade in propriedades)
+        {
+            propiedade.SetColumnType("varchar(150)");
+        }
+
+        // ao iniciar esse projeto (migration) pegar todos dbset setar para mapeamento.
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+        var chavesExtragerias = modelBuilder.Model.GetEntityTypes()
+                                                            .SelectMany(e => e.GetForeignKeys());
+        foreach (var chave in chavesExtragerias)
+        {
+            // vamos impedir deletar filhos(cascata)
+            chave.DeleteBehavior = DeleteBehavior.ClientSetNull;
+        }
+
+        base.OnModelCreating(modelBuilder);
+    }
+
     public DbSet<Produto> Produtos { get; set; }
     public DbSet<Fornecedor> Fornecedores { get; set; }
     public DbSet<Endereco> Enderecos { get; set; }
