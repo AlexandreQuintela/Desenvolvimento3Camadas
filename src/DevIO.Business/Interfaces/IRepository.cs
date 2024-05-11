cd7736aefc10cc36016dbe09f3348085befc8989
alexandrequintela <alexsquin@gmail.com>
2024-05-11 06:23:57 -0300

    Renomeado método SaveChanges para SaveChangesAsync na interface

--- src/DevIO.Business/Interfaces/IRepository.cs
+++ src/DevIO.Business/Interfaces/IRepository.cs
@@ -11,5 +11,5 @@ public interface IRepository<TEntity> : IDisposable where TEntity : Entity
     Task Atualizar(TEntity entity);
     Task Remover(Guid id);
     Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate);
-    Task<int> SaveChanges();
+    Task<int> SaveChangesAsync();
 }
