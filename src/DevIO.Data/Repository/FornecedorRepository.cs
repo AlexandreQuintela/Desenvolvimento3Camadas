77787a112a8d7b96d5b8b5f48a863cee42c9c89e
alexandrequintela <alexsquin@gmail.com>
2024-05-13 21:50:14 -0300

    Criação do Fornecedor repository

---
+++ src/DevIO.Data/Repository/FornecedorRepository.cs
@@ -0,0 +1,41 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using DevIO.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Data.Repository;
+
+public class FornecedorRepository : Repository<Fornecedor>, IFornecedorRepository
+{
+    public FornecedorRepository(MeuDbContext context) : base(context)
+    {
+    }
+
+    public async Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId)
+    {
+        return await _Db.Enderecos.AsNoTracking()
+                                    .FirstOrDefaultAsync(e => e.FornecedorID == fornecedorId);
+    }
+
+    public async Task<Fornecedor> ObterFornecedorEndereco(Guid id)
+    {
+        return await _Db.Fornecedores.AsNoTracking()
+                                        .Include(f => f.Endereco)
+                                        .FirstOrDefaultAsync(f => f.Id == id);
+    }
+
+    public async Task<Fornecedor> ObterFornecedorProdutosEndereco(Guid id)
+    {
+        return await _Db.Fornecedores.AsNoTracking()
+                                        .Include(f => f.Produtos)
+                                        .Include(f => f.Endereco)
+                                        .FirstOrDefaultAsync(f => f.Id == id);
+    }
+
+    public async Task RemoverEnderecoFornecedor(Endereco endereco)
+    {
+        _Db.Enderecos.Remove(endereco);
+        await SaveChangesAsync();
+    }
+}
+
