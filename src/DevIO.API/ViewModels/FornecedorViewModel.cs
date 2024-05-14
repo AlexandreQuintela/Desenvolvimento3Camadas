2aaa2f212e122c3c699ed27470266b5649f390e5
alexandrequintela <alexsquin@gmail.com>
2024-05-13 22:29:38 -0300

    Criado o FornecedorViewModel

---
+++ src/DevIO.API/ViewModels/FornecedorViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevIO.API.ViewModels;
+
+public class FornecedorViewModel
+{
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "O Campo {0} é obrigatório.")]
+    [StringLength(100, MinimumLength = 2, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} Caracteres.")]
+    public string? Nome { get; set; }
+
+    [Required(ErrorMessage = "O Campo {0} é obrigatório.")]
+    [StringLength(14, MinimumLength = 11, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} Caracteres.")]
+    public string? Documento { get; set; }
+    public int TipoFornecedor { get; set; }
+    public bool Ativo { get; set; }
+    public EnderecoViewModel? Endereco { get; set; }
+    public IEnumerable<ProdutoViewModel>? Produtos { get; set; }
+}
