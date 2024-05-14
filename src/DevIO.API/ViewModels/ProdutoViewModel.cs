3fc362ee6c8ba47f2bba3ac106c1d4bd1a9838c8
alexandrequintela <alexsquin@gmail.com>
2024-05-13 22:17:27 -0300

    Criado o ProdutoViewModel

---
+++ src/DevIO.API/ViewModels/ProdutoViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevIO.API.ViewModels;
+
+public class ProdutoViewModel
+{
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "O Campo {0} é obrigatório.")]
+    [StringLength(200, MinimumLength = 2, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} Caracteres.")]
+    public string? Nome { get; set; }
+
+    [Required(ErrorMessage = "O Campo {0} é obrigatório.")]
+    [StringLength(100, MinimumLength = 2, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} Caracteres.")]
+    public string? Descricao { get; set; }
+
+    [Required(ErrorMessage = "O Campo {0} é obrigatório.")]
+    public decimal Valor { get; set; }
+    public DateTime DataCadastro { get; set; }
+    public bool Ativo { get; set; }
+
+    [Required(ErrorMessage = "O Campo {0} é obrigatório.")]
+    public Guid FornecedorId { get; set; }
+
+    public string NomeFornecedor { get; set; }
+}
