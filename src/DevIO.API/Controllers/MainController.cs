445978be11547e545d836d461f27d5751458e3e1
alexandrequintela <alexsquin@gmail.com>
2024-05-21 00:20:35 -0300

    Estrutura básica para retornar alguma coisa.

--- src/DevIO.API/Controllers/MainController.cs
+++ src/DevIO.API/Controllers/MainController.cs
@@ -1,9 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace DevIO.API.Controllers;
 
 [ApiController]
 public abstract class MainController : ControllerBase
 {
+    protected bool OperacaoValida()
+    {
+        return true;
+    }
+
+    protected ActionResult RespostaPadrao(object result = null)
+    {
+        return OperacaoValida()
+            ? new ObjectResult(result)
+            : (ActionResult)BadRequest(new
+            {
+                // Todo: Coleção erros
+            });
+    }
+
+    protected ActionResult RespostaPadrao(ModelStateDictionary modelState)
+    {
+        if (!modelState.IsValid)
+        {
+            // Todo: Notifica Erros
+        }
+        return RespostaPadrao();
+    }
 
 }
