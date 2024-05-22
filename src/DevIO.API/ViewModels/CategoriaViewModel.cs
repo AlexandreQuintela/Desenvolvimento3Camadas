fc22a75154d97a422eca32935ac6a01e8b5e6d5e
alexandrequintela <alexsquin@gmail.com>
2024-05-22 01:19:04 -0300

    Criação da viewmodel categoria

---
+++ src/DevIO.API/ViewModels/CategoriaViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevIO.API.ViewModels
+{
+    public class CategoriaViewModel
+    {
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "O Campo {0} é obrigatório.")]
+        [StringLength(60, MinimumLength = 2, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} Caracteres.")]
+        public String Nome { get; set; } = string.Empty;
+    }
+}
