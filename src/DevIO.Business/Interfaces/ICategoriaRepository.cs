8560adc84a44a61acc36213ebe2223039ea4e217
alexandrequintela <alexsquin@gmail.com>
2024-05-22 00:18:26 -0300

    Criada interface categoria repository e implementada

---
+++ src/DevIO.Business/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace DevIO.Business.Interfaces;
+
+public interface ICategoriaRepository : IRepository<Categoria>
+{
+    Task<Categoria> ObterProdutoCategoria(Guid id);
+}
---
+++ src/DevIO.Data/Repository/CategoriaRepository.cs
@@ -0,0 +1,16 @@
+﻿
+namespace DevIO.Data.Repository;
+
+public class CategoriaRepository : Repository<Categoria>, ICategoriaRepository
+{
+    public CategoriaRepository(MeuDbContext context) : base(context)
+    {
+    }
+
+    public async Task<Categoria> ObterProdutoCategoria(Guid id)
+    {
+        return await _Db.Categorias.AsNoTracking()
+            .Include(p => p.Produtos)
+            .FirstOrDefaultAsync(c => c.Id == id);
+    }
+}
