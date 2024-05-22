db976c5a0bb0ce9270490474384165325a52e48a
alexandrequintela <alexsquin@gmail.com>
2024-05-22 01:54:02 -0300

    Injeção de dependência da categoria

--- src/DevIO.API/Configurations/DependencyInjectionConfig.cs
+++ src/DevIO.API/Configurations/DependencyInjectionConfig.cs
@@ -15,11 +15,13 @@ public static class DependencyInjectionConfig
         // Scopped é usado para aplicações web a criação de uma instancia ele é utilizada todo o cliclo do request
         services.AddScoped<IProdutoRepository, ProdutoRepository>();
         services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+        services.AddScoped<ICategoriaRepository, CategoriaRepository>();
 
         //business
         services.AddScoped<IFornecedorService, FornecedorService>();
         services.AddScoped<IProdutoService, ProdutoService>();
         services.AddScoped<INotificador, Notificador>();
+        services.AddScoped<ICategoriaService, CategoriaService>();
 
         return services;
     }
